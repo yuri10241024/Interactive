97257dcaaff5e34d6fb66f822625ad111775d639
8fujisoul <112898726+8fujisoul@users.noreply.github.com>
2023-06-13 20:11:50 +0300

    Например Лёвин 13 июля

---
+++ Lev S test/Interactive moive/AboutUs.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Interactive_moive
+{
+    /// <summary>
+    /// Interaction logic for AboutUs.xaml
+    /// </summary>
+    public partial class AboutUs : Window
+    {
+        public AboutUs()
+        {
+            InitializeComponent();
+            ReadAboutUs(@"C:\Lev S test\AboutUs\AboutUs.txt");
+        }
+
+        void ReadAboutUs(string path)
+        {
+            string[] str = File.ReadAllLines(path);
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                TBAboutUs.Text = str[i];
+            }
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            //MainWindow mainWindow = new MainWindow();
+            //mainWindow.ShowDialog();
+        }
+    }
+}
---
+++ Lev S test/Interactive moive/GameWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Interactive_moive
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public Window parent = null;//инфа о любом окне.
+
+        public GameWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (parent != null)
+            {
+                parent.Close();
+            }
+        }
+
+        private void Border_MouseDown3(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Здравствуйте мир!");
+        }
+
+        private void Border_MouseDown2(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Здравствуйте мир2!");
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Здравствуйте мир3!");
+        }
+    }
+}
