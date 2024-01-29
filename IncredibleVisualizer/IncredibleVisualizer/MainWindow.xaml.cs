370659e0307f30ee1156bb4d9267ef435528f6e2
8fujisoul <112898726+8fujisoul@users.noreply.github.com>
2024-01-29 21:30:22 +0300

    29.01.24

--- IncredibleVisualizer/IncredibleVisualizer/MainWindow.xaml.cs
+++ IncredibleVisualizer/IncredibleVisualizer/MainWindow.xaml.cs
@@ -22,5 +22,23 @@ namespace IncredibleVisualizer
             InitializeComponent();
 
         }
+
+        private void DrawLine(SceneBlock sceneBlock1, SceneBlock sceneBlock2)
+        {
+            double Top1 = Canvas.GetTop(sceneBlock1);
+            double Left1 = Canvas.GetLeft(sceneBlock1);
+            double Top2 = Canvas.GetTop(sceneBlock2);
+            double Left2 = Canvas.GetLeft(sceneBlock2);
+            double y1 = Top1 + 60;
+            double y2 = Top2 + 60;
+            double x1 = Left1 + 106;
+            double x2 = Left2 + 106;
+            Line line = new Line();
+            line.X1 = x1;
+            line.X2 = x2;
+            line.Y1 = y1;
+            line.Y2 = y2;
+            //VelikijCanvasDrevnihRusov Children.
+        }
     }
 }
